de552c5593773e9b89c570dee2635ef1a612e0d8
Vaibhav Labhane <labhane@gmail.com>
2022-03-28 13:00:23 +1030

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I was changed !
         </p>
         <a
           className="App-link"
